d36346a0881e9631abbad8a0abf47ad184fef8bf
Joshua <97867490+ccamel55@users.noreply.github.com>
2022-07-25 01:04:43 +1200

    fixed scrolling on body div

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -37,6 +37,7 @@ export default function Body() {
 
                 overflowY: "auto",
                 width: "100%",
+                height: "100vh",
                 alignItems: "center",
                 gap: "50px",
             }}>
